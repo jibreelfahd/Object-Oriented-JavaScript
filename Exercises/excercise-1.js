46f8ebfc38cdb8b68e20da11a9417a6e515677fe
Fahd Jibril <jibreelfahd@gmail.com>
2025-01-07 13:59:47 +0100

    Second Commit

---
+++ Exercises/excercise-1.js
@@ -0,0 +1,57 @@
+/* 
+
+1. Create an Animal class. The class will have name, age, color, legs properties and create different methods
+2. Create a Dog and Cat child class from the Animal Class. 
+
+*/
+
+class Animal {
+  constructor(name, age, color, legs) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.legs = legs;
+  }
+
+  description() {
+    console.log(
+      `My name is: ${this.name}, i am ${this.age} years old, i have ${this.legs} legs and i am ${this.color} in complexion`
+    ); //i dont know how to spell the word 😂
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, color) {
+    super(name, age, color);
+    this.legs = 4;
+    this.bark = "I bark anytime i need food";
+  }
+
+  description() {
+    console.log(
+      `My name is: ${this.name}, i am ${this.age} years old, i have ${this.legs} legs, i am ${this.color} and ${this.bark}.`
+    );
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, color) {
+    super(name, age, color);
+    this.legs = 4;
+    this.meow = "My meow is cute";
+  }
+
+  description() {
+    console.log(
+      `My name is: ${this.name}, i am ${this.age} years old, i have ${this.legs} legs and i am ${this.color}. FYI, ${this.meow}.`
+    );
+  }
+}
+
+const animalOne = new Animal("Bird", 4, "dark", 2);
+const dogOne = new Dog("Bingo", 5, "white");
+const catOne = new Cat("Damian", 9, "golden");
+
+animalOne.description();
+dogOne.description();
+catOne.description();
---
+++ Exercises/exercise-2.js
@@ -0,0 +1,86 @@
+/* 
+
+Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+*/
+class PersonAccount {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.incomes = [];
+    this.expenses = [];
+  }
+
+  calculateTotalIncome() {
+    let sum = 0;
+
+    for (const allIncome of this.incomes) {
+      sum += allIncome;
+    }
+    return sum;
+  }
+
+  calculateTotalExpense() {
+    let sum = 0;
+
+    for (const allExpenes of this.expenses) {
+      sum += allExpenes;
+    }
+    return sum;
+  }
+
+  totalIncome(description) {
+    const date = new Date();
+
+    console.log(
+      `Total Income for ${date.getDate()} is: ${this.calculateTotalIncome()}, description for total Income is: ${description}`
+    );
+  }
+
+  totalExpense(description) {
+    const date = new Date();
+
+    console.log(
+      `Total Expense for ${date.getDate()} is: ${this.calculateTotalExpense()}, description for total Expense is: ${description}`
+    );
+  }
+
+  accountInfo() {
+    const date = new Date();
+    const incomes = this.incomes.join(", ");
+    const expenses = this.expenses.join(", ");
+    console.log(
+      `First Name: ${this.firstname}\nLast Name: ${
+        this.lastname
+      }\nTotal Income (${date.getMonth()}): ${this.calculateTotalIncome()}\nTotal Expenses (${date.getMonth()}): ${this.calculateTotalExpense()}`
+    );
+  }
+
+  addIncome(incomeOne, incomeTwo, incomeThree) {
+    this.incomes.push(incomeOne, incomeTwo, incomeThree);
+    console.log(
+      `Incomes added are: ${incomeOne}, ${incomeTwo}, ${incomeThree}`
+    );
+  }
+  addExpense(expenseOne, expenseTwo, expenseThree) {
+    this.expenses.push(expenseOne, expenseTwo, expenseThree);
+    console.log(
+      `Expenses added are: ${expenseOne}, ${expenseTwo}, ${expenseThree}`
+    );
+  }
+  accountBalance() {
+    const totalIncome = this.calculateTotalIncome();
+    const totalExpense = this.calculateTotalExpense();
+
+    const totalBalance = totalIncome - totalExpense;
+    console.log("Total Account Balance is", totalBalance);
+  }
+}
+
+const personOne = new PersonAccount("John", "Doe");
+personOne.addIncome(2000, 5000, 4000);
+personOne.addExpense(1000, 900, 1500);
+personOne.accountBalance();
+// personOne.totalIncome("Money gotten from all the small gigs i took");
+// personOne.totalExpense('Money i spent on data');
+// personOne.accountInfo();
