6442523860a8a0640ac55ba07fa05080cacd87b7
Fahd Jibril <jibreelfahd@gmail.com>
2025-01-08 01:45:20 +0100

    Exercise-3

---
+++ Exercises/exercise-3.js
@@ -0,0 +1,64 @@
+/* 
+Write a function that is going create a deck of cards and also perfom operations like initialize the card, shuffle the card, draw the card and reset the card
+
+*/
+
+//  @desc: PSEUDOCODE FOR THE PROBLEM
+//  in a card i have suits and also ranks
+//  the suit include 'Heart' 'Spades' 'Diamonds' 'Clubs';
+//  a rank includes the '2, 3, 4, 5, 6, 7, 8, 9, 10, Ace, Jack, Queen, King';
+//  initialize the cards to include all the suits
+//  create a method that changes the position of all cards
+//  create a method that draws one of the card from its position
+//  reset the card
+
+class Cards {
+  constructor(suit, rank) {
+    this.suit = suit;
+    this.rank = rank;
+  }
+
+  toString() {
+    return `Card: ${this.rank} of ${this.suit}`;
+  }
+}
+
+class Deck {
+  constructor() {
+    this.suit = ["Heart", "Spades", "Diamonds", "Clubs"];
+    this.rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10","Ace", "Jack", "Queen", "King"];
+    this.cards = [];
+    this.initializeDeck();
+  }
+
+  initializeDeck() {
+    this.cards = [] // mainly used when the reset function is being called
+    for (const suit of this.suit) {
+      for (const rank of this.rank) {
+        this.cards.push(new Cards(suit, rank))
+      }
+    }
+  }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  draw() {
+    return this.cards.pop();
+  }
+
+  reset() {
+    this.initializeDeck();
+    this.shuffle();
+  }
+}
+
+const deckOne = new Deck();
+deckOne.shuffle()
+console.log(deckOne.draw().toString())
+console.log(deckOne.draw().toString())
+console.log(deckOne.cards.length)
