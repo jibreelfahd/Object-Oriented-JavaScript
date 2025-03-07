4c4d5708f7897d51e34e5cf400b3899aeda5d325
Fahd Jibril <jibreelfahd@gmail.com>
2025-03-07 02:08:06 +0100

    solved two exercises in exercise-6.ts

--- Exercises/src/exercise-6.ts
+++ Exercises/src/exercise-6.ts
@@ -1,7 +1,9 @@
 /* 
 1. Write a function to find the largest number in an array.
 2. How would you flatten a nested array (e.g., [1, [2, [3, 4]]] → [1, 2, 3, 4])?
-3. sorts an array from ascending to descending deck of cards = ['Jack', 8, 2, 2, 6, 'King', 5, 3, 'Queen', 'King', 'Queen']
+3. Sorts an array from ascending to descending deck of cards = ['Jack', 8, 2, 2, 6, 'King', 5, 3, 'Queen', 'King', 'Queen']
+4. Implement a function to remove duplicates from an array.
+5. Write a function to merge two sorted arrays into one sorted array.
 */
 
 // first question
@@ -40,25 +42,101 @@ const flattenArray = (arr: NestedArray[]) => {
 console.log(flattenArray(nestedArray));
 
 //third question
-const cards: (string | number)[] = ['Jack', 8, 2, 2, 6, 'King', 5, 3, 'Queen', 'King', 'Queen'];
+const cards: (string | number)[] = [
+  "Jack",
+  8,
+  2,
+  2,
+  6,
+  "King",
+  5,
+  3,
+  "Queen",
+  "King",
+  "Queen",
+];
 
 const sortString = () => {
   let stringArray: string[] = [];
   let numberArray: number[] = [];
 
   for (let i = 0; i < cards.length; i++) {
-    if (typeof cards[i] === 'string') {
-      stringArray.push((cards[i] as string));
-    } else if(typeof cards[i] === 'number') {
-      numberArray.push((cards[i] as number));
+    if (typeof cards[i] === "string") {
+      stringArray.push(cards[i] as string);
+    } else if (typeof cards[i] === "number") {
+      numberArray.push(cards[i] as number);
     }
   }
 
   const sortedString = stringArray.sort();
   const sortedNumbers = numberArray.sort((a, b) => a - b);
-  
+
   const newArray: (string | number)[] = [...sortedString, ...sortedNumbers];
   return newArray;
 };
 
 console.log(sortString());
+
+//fourth question
+const numArray = [2, 3, 4, 2, 5, 5, 4, 6];
+
+const removeDuplicates = (arr: number[]) => {
+  const noDuplicateArray: number[] = [];
+
+  for (const item of arr) {
+    if (!noDuplicateArray.includes(item)) {
+      noDuplicateArray.push(item);
+    }
+  }
+  return noDuplicateArray;
+};
+
+console.log(removeDuplicates(numArray));
+
+// fifth question
+function mergeArrays<T extends number | string>(arrayOne: T[], arrayTwo: T[]) {
+  const checkArrayOneType = arrayOne.every((item) => typeof item === "string");
+
+  const sortedArrayOne = checkArrayOneType
+    ? (arrayOne as string[]).sort()
+    : (arrayOne as number[]).sort((a: number, b: number) => a - b);
+
+  const checkArrayTwoType = arrayTwo.every((item) => typeof item === "string");
+
+  const sortedArrayTwo = checkArrayTwoType
+    ? (arrayTwo as string[]).sort()
+    : (arrayTwo as number[]).sort((a: number, b: number) => a - b);
+
+  let i = 0,
+    j = 0;
+  const mergeArrayVal = [];
+
+  while (i < sortedArrayOne.length && j < sortedArrayTwo.length) {
+    if (sortedArrayOne[i] > sortedArrayTwo[j]) {
+      mergeArrayVal.push(sortedArrayTwo[j]);
+      j++
+    } else {
+      mergeArrayVal.push(sortedArrayOne[i]);
+      i++;
+    }
+  }
+
+  while (i < sortedArrayOne.length) {
+    mergeArrayVal.push(sortedArrayOne[i]);
+    i++;
+  }
+
+  while (j < sortedArrayTwo.length) {
+    mergeArrayVal.push(sortedArrayTwo[j]);
+    j++
+  }
+
+  return mergeArrayVal;
+}
+
+console.log(
+  mergeArrays<string | number>(
+    ["Jack", "King", "Queen", "King", "Queen"],
+    [8, 2, 2, 6, 5, 3]
+  )
+);
